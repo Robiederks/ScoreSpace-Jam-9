2fbb2a3e28f24f780d9e7a90b562fbe2a511b803
Robiederks <trebor.21@hotmail.com>
2020-07-25 23:46:10 +0200

    Collectable

---
+++ src/nl/ru/science/student/kunst/r/scoreSpaceJam9/Collectable.java
@@ -0,0 +1,64 @@
+package nl.ru.science.student.kunst.r.scoreSpaceJam9;
+
+public abstract class Collectable extends Entity {
+
+	private int state;
+	private int timer;
+	
+	public Collectable(int x, World world) {
+		super(x, -4, 24, 24, world);
+		state = 0;
+	}
+
+	@Override
+	public void tick() {
+		if (state == 0) {
+			pixelY += 1;
+			if (pixelY == Game.HEIGHT - World.STEP_HEIGHT * world.getWallHeight() - 49) {
+				state = 1;
+			}
+		}
+		else {
+			timer++;
+			if (timer == 10) {
+				state = (state % 8) + 1;
+				timer = 0;
+			}
+			switch (state) {
+			case 1:
+				pixelY += 1;
+				break;
+			case 2:
+				if (timer % 2 == 0) {
+					pixelY += 1;
+				}
+				break;
+			case 3:
+				break;
+			case 4:
+				if (timer % 2 == 0) {
+					pixelY -= 1;
+				}
+				break;
+			case 5:
+				pixelY -= 1;
+				break;
+			case 6:
+				if (timer % 2 == 0) {
+					pixelY -= 1;
+				}
+				break;
+			case 7:
+				break;
+			case 8:
+				if (timer % 2 == 0) {
+					pixelY += 1;
+				}
+				break;
+			}
+		}
+	}
+	
+	public abstract void use();
+
+}
